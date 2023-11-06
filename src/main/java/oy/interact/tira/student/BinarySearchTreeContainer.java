909a10eaefe71898c7618a878065b9f2deed4834
aake48 <131197767+aake48@users.noreply.github.com>
2023-11-06 18:38:45 +0200

    TASK07

--- src/main/java/oy/interact/tira/student/BinarySearchTreeContainer.java
+++ src/main/java/oy/interact/tira/student/BinarySearchTreeContainer.java
@@ -12,6 +12,7 @@ public class BinarySearchTreeContainer<K extends Comparable<K>, V> implements TI
     private TreeNode<K, V> root = null;
     private int count = 0;
     private int maxDepth = 0;
+
     /**
      * Adds a key-value pair in the container. Key must be unique, so
      * if a key that equals new key already exists in the container, replace
@@ -26,7 +27,7 @@ public class BinarySearchTreeContainer<K extends Comparable<K>, V> implements TI
     @Override
     public void add(K key, V value) throws OutOfMemoryError, IllegalArgumentException {
 
-        if (key == null || value == null){
+        if (key == null || value == null) {
             throw new IllegalArgumentException("Error: Key or value is null");
         }
         if (root == null) {
@@ -69,59 +70,58 @@ public class BinarySearchTreeContainer<K extends Comparable<K>, V> implements TI
         throw new UnsupportedOperationException("Unimplemented method 'remove'");
     }
 
-	/**
-	 * Searchers for a value conforming to the predicate in the parameter.
-	 * 
-	 * Returns the first value that conforms to the predicate.
-	 * 
-	 * @param searcher The predicate that is used in the search.
-	 * @return The value conforming to the predicate, or null if not found.
-	 */
+    /**
+     * Searchers for a value conforming to the predicate in the parameter.
+     * 
+     * Returns the first value that conforms to the predicate.
+     * 
+     * @param searcher The predicate that is used in the search.
+     * @return The value conforming to the predicate, or null if not found.
+     */
     @Override
     public V find(Predicate<V> searcher) {
 
-        if (root == null){
+        if (root == null) {
             return null;
         }
         V value = null;
         TreeNode<K, V> current = root;
-        TreeNode<K, V> parent = null;        
+        TreeNode<K, V> parent = null;
         StackInterface<TreeNode<K, V>> nodeStack = new StackImplementation<>();
 
-        while (!nodeStack.isEmpty() || current != null){
-            if (current != null){
+        while (!nodeStack.isEmpty() || current != null) {
+            if (current != null) {
                 nodeStack.push(current);
                 parent = current;
                 current = current.getLeftChild();
             } else {
                 parent = nodeStack.pop();
                 current = parent.getRightChild();
-                if (searcher.test(parent.getValue())){
+                if (searcher.test(parent.getValue())) {
                     value = parent.getValue();
                 }
             }
         }
         return value;
 
-
     }
 
-
     /**
-	 * The number of key-value pairs in the container.
-	 * 
-	 * @return The number of pairs in container.
-	 */
+     * The number of key-value pairs in the container.
+     * 
+     * @return The number of pairs in container.
+     */
     @Override
     public int size() {
         return count;
     }
-	/**
-	 * The total capacity of the container, indicating how many elements
-	 * the container can hold currently.
-	 * 
-	 * @return The capacity of the container.
-	 */
+
+    /**
+     * The total capacity of the container, indicating how many elements
+     * the container can hold currently.
+     * 
+     * @return The capacity of the container.
+     */
     @Override
     public int capacity() {
         return maxDepth;
@@ -140,30 +140,30 @@ public class BinarySearchTreeContainer<K extends Comparable<K>, V> implements TI
         maxDepth = 0;
     }
 
-    	/**
-	 * Provides the elements of the container in an array. Note that
-	 * the array must not be any internal array used by the container, but
-	 * a temporary array allocated in the method. Caller can then manipulate
-	 * that array without manipulating the container internal data.
-	 * 
-	 * @return The key-value pairs in the container in an array.
-	 * @throws Exception If memory runs out or other faults happen in creating the array.
-	 */
+    /**
+     * Provides the elements of the container in an array. Note that
+     * the array must not be any internal array used by the container, but
+     * a temporary array allocated in the method. Caller can then manipulate
+     * that array without manipulating the container internal data.
+     * 
+     * @return The key-value pairs in the container in an array.
+     * @throws Exception If memory runs out or other faults happen in creating the
+     *                   array.
+     */
     @Override
-    @SuppressWarnings ("unchecked")
+    @SuppressWarnings("unchecked")
     public Pair<K, V>[] toArray() throws Exception {
 
-        Pair<K, V> [] array = (Pair<K, V>[]) new Pair[count];
-        if (root == null){
+        Pair<K, V>[] array = (Pair<K, V>[]) new Pair[count];
+        if (root == null) {
             return array;
         }
-        int arrayIndex [] = {0};
+        int arrayIndex[] = { 0 };
 
-        
         root.toArray(array, arrayIndex[0]);
-        
+
         return array;
-        
+
     }
 
     /**
@@ -198,7 +198,7 @@ public class BinarySearchTreeContainer<K extends Comparable<K>, V> implements TI
             } else {
                 parent = nodeStack.pop();
                 current = parent.getRightChild();
-                if (parent.getKey().equals(itemKey)){
+                if (parent.getKey().equals(itemKey)) {
                     return index;
                 }
                 index++;
@@ -208,80 +208,75 @@ public class BinarySearchTreeContainer<K extends Comparable<K>, V> implements TI
         return -1;
     }
 
-    	/**
-	 * Retrieves the key-value pair at the index.
-	 * 
-	 * @param index Index to retrieve, must be 0..<size.
-	 * @return The key-value pair.
-	 * @throws IndexOutOfBoundsException If index < 0 or index >= size.
-	 */
+    /**
+     * Retrieves the key-value pair at the index.
+     * 
+     * @param index Index to retrieve, must be 0..<size.
+     * @return The key-value pair.
+     * @throws IndexOutOfBoundsException If index < 0 or index >= size.
+     */
     @Override
     public Pair<K, V> getIndex(int index) throws IndexOutOfBoundsException {
 
-        if (index < 0 || index >= size()){
+        if (index < 0 || index >= size()) {
             throw new IndexOutOfBoundsException("Error: Index out of bounds");
         }
-        int currentIndex = 0;        
-        Pair<K, V> value = null;
+        int currentIndex = 0;
         TreeNode<K, V> current = root;
-        TreeNode<K, V> parent = null;   
+        TreeNode<K, V> parent = null;
         StackInterface<TreeNode<K, V>> nodeStack = new StackImplementation<>();
 
-        if (root == null){
+        if (root == null) {
             return null;
         }
 
-        while (!nodeStack.isEmpty() || current != null){
-            if (current != null){
+        while (!nodeStack.isEmpty() || current != null) {
+            if (current != null) {
                 nodeStack.push(current);
                 parent = current;
                 current = current.getLeftChild();
-            }else {
+            } else {
                 parent = nodeStack.pop();
                 current = parent.getRightChild();
-                if(index == currentIndex){
-                    value = new Pair<>(current.getKey(), current.getValue());
+                if (index == currentIndex) {
+                    return new Pair<>(parent.getKey(), parent.getValue());
                 }
+                currentIndex++;
             }
-            currentIndex++;
+
         }
 
+        return null;
 
-        
-        
-
-        
-        
-        return value;
-       
     }
-	/**
-	 * Finds an index for the value conforming to the predicate.
-	 * 
-	 * @param searcher The predicate specifying the search criteria.
-	 * @return The index for the value or -1 if not found.
-	 */
+
+    /**
+     * Finds an index for the value conforming to the predicate.
+     * 
+     * @param searcher The predicate specifying the search criteria.
+     * @return The index for the value or -1 if not found.
+     */
     @Override
     public int findIndex(Predicate<V> searcher) {
         StackInterface<TreeNode<K, V>> stack = new StackImplementation<>();
         TreeNode<K, V> current = root;
         TreeNode<K, V> parent = null;
         int index = 0;
-        
-        while(!stack.isEmpty() || root != null){
-            if (current != null){
+
+        while (!stack.isEmpty() || root != null) {
+            if (current != null) {
                 stack.push(current);
                 parent = current;
                 current = current.getLeftChild();
             } else {
                 parent = stack.pop();
                 current = parent.getRightChild();
-                if (searcher.test(root.getValue())){
+                if (searcher.test(root.getValue())) {
                     return index;
                 }
                 index++;
             }
-        }   
+        }
         return -1;
     }
 
